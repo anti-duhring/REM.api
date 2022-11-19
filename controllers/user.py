f423819bc5314105ab63d7171adf1647aedc2f5d
Mateus Vinícius <mateusvnlima@gmail.com>
2022-11-19 03:05:34 +0000

    fix: CRUD dos usuários

--- controllers/user.py
+++ controllers/user.py
@@ -3,36 +3,38 @@ import json
 import collections
 from flask import make_response
 
+
 class UserController:
-    def __init__(self):
-        print('Initialize user controller')
 
-    def create(self, data):
-        ConnectDB().connect()
-        res = {}
+  def __init__(self):
+    print('Initialize user controller')
 
-        token = data['token']
-        admin = data['admin']
-        access = data['access']
-        name = data['name']
+  def create(self, data):
+    ConnectDB().connect()
+    res = {}
 
-        command = f'INSERT INTO Users (token, admin, access, name) VALUES ("{token}", {admin}, {access}, "{name}")'
+    token = data['token']
+    admin = data['admin']
+    access = data['access']
+    name = data['name']
 
-        try:
-            ConnectDB().exec_DML(sql=command)
-            res['user'] = self.show_by_token(token=token)
+    command = f'INSERT INTO Users (token, admin, access, name) VALUES ("{token}", {admin}, {access}, "{name}")'
 
-            return make_response(res, 201)
-        except Exception as e:
-            print(e)
-            res['message'] = 'Erro ao criar usuário usuário'
-            return make_response(res, 401)
-    
-    def index(self):
-        ConnectDB().connect()
-        res = {}
+    try:
+      ConnectDB().exec_DML(sql=command)
+      res['user'] = self.show_by_token(token=token, format='json')
 
-        command = f'''SELECT 
+      return make_response(res, 201)
+    except Exception as e:
+      print(e)
+      res['message'] = 'Erro ao criar usuário usuário'
+      return make_response(res, 401)
+
+  def index(self):
+    ConnectDB().connect()
+    res = {}
+
+    command = f'''SELECT 
             id, 
             token, 
             name,
@@ -40,28 +42,28 @@ class UserController:
             access 
         FROM Users'''
 
-        try:
-            result = ConnectDB().exec_DQL(sql=command)
-            res["users"] = []
-            for row in result:
-                d = collections.OrderedDict()
-                d["id"] = row[0]
-                d["token"] = row[1]
-                d["name"] = row[2]
-                d["admin"] = row[3]
-                d["access"] = row[4]
-                res["users"].append(d)
-            
-            return make_response(res, 200)
-        except Exception as e:
-            print(e)
-            res['message'] = 'Erro ao retornar usuários'
-            return make_response(res, 400)
-        
-    def show(self, user_id):
-        ConnectDB().connect()
-        res = {}
-        command = f'''SELECT 
+    try:
+      result = ConnectDB().exec_DQL(sql=command)
+      res["users"] = []
+      for row in result:
+        d = collections.OrderedDict()
+        d["id"] = row[0]
+        d["token"] = row[1]
+        d["name"] = row[2]
+        d["admin"] = row[3]
+        d["access"] = row[4]
+        res["users"].append(d)
+
+      return make_response(res, 200)
+    except Exception as e:
+      print(e)
+      res['message'] = 'Erro ao retornar usuários'
+      return make_response(res, 400)
+
+  def show(self, user_id, format='response'):
+    ConnectDB().connect()
+    res = {}
+    command = f'''SELECT 
             id, 
             token, 
             name,
@@ -69,82 +71,85 @@ class UserController:
             access 
         FROM Users WHERE id = {user_id}'''
 
-        try:
-            result = ConnectDB().exec_DQL(sql=command)
-            res['user'] = {}
-            for row in result:
-                res['user']["id"] = row[0]
-                res['user']["token"] = row[1]
-                res['user']["name"] = row[2]
-                res['user']["admin"] = row[3]
-                res['user']["access"] = row[4]
-            
-            return make_response(res, 200)
-        except Exception as e:
-            print(e)
-            res['message'] = 'Erro ao retornar usuários'
-            return make_response(res, 400)
+    try:
+      result = ConnectDB().exec_DQL(sql=command)
+      res['user'] = {}
+      for row in result:
+        res['user']["id"] = row[0]
+        res['user']["token"] = row[1]
+        res['user']["name"] = row[2]
+        res['user']["admin"] = row[3]
+        res['user']["access"] = row[4]
 
-    def show_by_token(self, token):
-        ConnectDB().connect()
-        res = {}
-        command = f'''SELECT 
+      if format == 'json':
+        return res['user']
+      else:
+        return make_response(res, 200)
+    except Exception as e:
+      print(e)
+      res['message'] = 'Erro ao retornar usuários'
+      return make_response(res, 400)
+
+  def show_by_token(self, token, format='response'):
+    ConnectDB().connect()
+    res = {}
+    command = f'''SELECT 
             id, 
             token, 
             name,
             admin,
             access 
-        FROM Users WHERE token = {token}'''
+        FROM Users WHERE token = "{token}"'''
 
-        try:
-            result = ConnectDB().exec_DQL(sql=command)
-            res['user'] = {}
-            for row in result:
-                res['user']["id"] = row[0]
-                res['user']["token"] = row[1]
-                res['user']["name"] = row[2]
-                res['user']["admin"] = row[3]
-                res['user']["access"] = row[4]
-            
-            return make_response(res, 200)
-        except Exception as e:
-            print(e)
-            res['message'] = 'Erro ao retornar usuários'
-            return make_response(res, 400)     
+    try:
+      result = ConnectDB().exec_DQL(sql=command)
+      res['user'] = {}
+      for row in result:
+        res['user']["id"] = row[0]
+        res['user']["token"] = row[1]
+        res['user']["name"] = row[2]
+        res['user']["admin"] = row[3]
+        res['user']["access"] = row[4]
 
-    def update(self, user_id, data):
-        ConnectDB().connect()
-        res = {}
-        admin = data['admin']
-        access = data['access']
-        name = data['name']
+      if format == 'json':
+        return res['user']
+      else:
+        return make_response(res, 200)
+    except Exception as e:
+      print(e)
+      res['message'] = 'Erro ao retornar usuários'
+      return make_response(res, 400)
 
-        command = f'UPDATE Users SET name = "{name}", access = {access}, admin = {admin} WHERE id = {user_id}'
+  def update(self, user_id, data):
+    ConnectDB().connect()
+    res = {}
+    admin = data['admin']
+    access = data['access']
+    name = data['name']
 
-        try:
-            ConnectDB().exec_DML(sql=command)
-            res['user'] = self.show(user_id=user_id)
+    command = f'UPDATE Users SET name = "{name}", access = {access}, admin = {admin} WHERE id = {user_id}'
 
-            return make_response(res, 201)
-        except Exception as e:
-            print(e)
-            res['message'] = 'Erro ao criar usuário usuário'
-            return make_response(res, 400)
-    
-    def delete(self, user_id):
-        ConnectDB().connect()
-        res = {
-            "message": f'Usuário {user_id} deletado com sucesso'
-        }
-        command = f'DELETE FROM Users WHERE id = {user_id}'
-        
-        try:
-            ConnectDB().exec_DML(sql=command)
+    try:
+      ConnectDB().exec_DML(sql=command)
+      res['user'] = self.show(user_id=user_id, format='json')
 
-            return make_response(res, 200)
-        except Exception as e:
-            print(e)
-            res['message'] = 'Erro ao deletar usuário'
+      return make_response(res, 201)
+    except Exception as e:
+      print(e)
+      res['message'] = 'Erro ao criar usuário usuário'
+      return make_response(res, 400)
 
-            return make_response(res, 400)
-        
+  def delete(self, user_id):
+    ConnectDB().connect()
+    res = {"message": f'Usuário {user_id} deletado com sucesso'}
+    command = f'DELETE FROM Users WHERE id = {user_id}'
+
+    try:
+      ConnectDB().exec_DML(sql=command)
+
+      return make_response(res, 200)
+    except Exception as e:
+      print(e)
+      res['message'] = 'Erro ao deletar usuário'
+
+      return make_response(res, 400)
--- main.py
+++ main.py
@@ -1,64 +1,72 @@
 from flask import Flask, make_response, jsonify, request
 from controllers.user import UserController
+from waitress import serve
 
 app = Flask('RemAPI')
 app.config['JSON_SORT_KEYS'] = False
 
+
+@app.after_request
+def apply_caching(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Methods',
+                       'GET,POST,OPTIONS,DELETE,PUT')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+  return response
+
+
 @app.route('/users', methods=['GET'])
 def show():
-    user_id = request.args.get('id')
+  user_id = request.args.get('id')
 
-    try:
-        if user_id:
-            result = UserController().show(user_id=user_id)
-        else:
-            result = UserController().index()
-        return result
-    except Exception as e:
-        print(e)
-        return make_response({
-            'message': 'Erro interno'
-        }, 500)
+  try:
+    if user_id:
+      result = UserController().show(user_id=user_id)
+    else:
+      result = UserController().index()
+
+    return result
+  except Exception as e:
+    print(e)
+    return make_response({'message': 'Erro interno'}, 500)
+
 
 @app.route('/users', methods=['POST'])
 def create():
-    request_data = request.get_json()
+  request_data = request.get_json()
 
-    try:
-        result = UserController().create(data=request_data)
-        return result
-    except Exception as e:
-        print(e)
-        return make_response({
-            'message': 'Erro interno'
-        }, 500)    
+  try:
+    result = UserController().create(data=request_data)
+    return result
+  except Exception as e:
+    print(e)
+    return make_response({'message': 'Erro interno'}, 500)
+
 
 @app.route('/users', methods=['DELETE'])
 def delete():
-    user_id = request.args.get('id')
+  user_id = request.args.get('id')
 
-    try:
-        result = UserController().delete(user_id=user_id)
-        return result
-    except Exception as e:
-        print(e)
-        return make_response({
-            'message': 'Erro interno'
-        }, 500)
+  try:
+    result = UserController().delete(user_id=user_id)
+    return result
+  except Exception as e:
+    print(e)
+    return make_response({'message': 'Erro interno'}, 500)
+
 
 @app.route('/users', methods=['PUT'])
 def update():
-    user_id = request.args.get('id')
-    request_data = request.get_json()
+  user_id = request.args.get('id')
+  request_data = request.get_json()
 
-    try:
-        result = UserController().update(user_id=user_id, data=request_data)
-        return result
-    except Exception as e:
-        print(e)
-        return make_response({
-            'message': 'Erro interno'
-        }, 500) 
+  try:
+    result = UserController().update(user_id=user_id, data=request_data)
+    return result
+  except Exception as e:
+    print(e)
+    return make_response({'message': 'Erro interno'}, 500)
+
 
 # app.run(debug=True)
-app.run()
+serve(app, host="0.0.0.0", port=8080)
