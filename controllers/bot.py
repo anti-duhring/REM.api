5cf4c374755646200e7c9f61acdbc8522504af84
anti-duhring <mateus.limavn@gmail.com>
2022-12-07 17:24:06 -0300

    feat: CRUD dos bots

--- controllers/bot.py
+++ controllers/bot.py
@@ -1,5 +1,7 @@
 import requests
 from requests.auth import HTTPBasicAuth
+from db import ConnectDB
+import collections
 
 class BotController:
 
@@ -24,3 +26,151 @@ class BotController:
             res['error'] = True
         
         return res
+    
+    def create(self, data):
+        ConnectDB().connect()
+        res = {
+            'error': False
+        }
+
+        title = data['title']
+        description = data['description']
+        type = data['type']
+        plataform = data['plataform']
+        link = data['link']
+
+        command = f'INSERT INTO bots (title, description, type, plataform, link) VALUES ("{title}", "{description}", "{type}", "{plataform}", "{link}")'
+
+        try:
+            ConnectDB().exec_DML(sql=command)
+            res['bot'] = self.show(bot_link=link)
+
+            return res
+        except Exception as e:
+            print(e)
+            res['error'] = True
+            res['message'] = 'Erro ao criar bot'
+            return res
+    
+    def show(self, bot_id = None, bot_link = None):
+        ConnectDB().connect()
+        res = {
+            'error': False,
+
+        }
+
+        if bot_id is not None:
+            query = f'id = {bot_id}'
+        else:
+            query = f'link = "{bot_link}"'
+
+        command = f'''SELECT 
+                id, 
+                title, 
+                description,
+                type,
+                plataform,
+                link 
+            FROM bots WHERE {query}'''
+
+        try:
+            result = ConnectDB().exec_DQL(sql=command)
+            res['bot'] = {}
+            for row in result:
+                res['bot']["id"] = row[0]
+                res['bot']["title"] = row[1]
+                res['bot']["description"] = row[2]
+                res['bot']["type"] = row[3]
+                res['bot']["plataform"] = row[4]
+                res['bot']["link"] = row[5]
+
+            return res['bot']
+        except Exception as e:
+            print(e)
+            res['error'] = True
+            res['message'] = 'Erro ao retornar bot'
+
+            return res
+    
+    def index(self):
+        ConnectDB().connect()
+        res = {
+            'error': False
+        }
+
+        command = f'''SELECT 
+                id, 
+                title, 
+                description,
+                type,
+                plataform,
+                link 
+            FROM bots'''
+
+        try:
+            result = ConnectDB().exec_DQL(sql=command)
+            res["bots"] = []
+            for row in result:
+                d = collections.OrderedDict()
+                d["id"] = row[0]
+                d["title"] = row[1]
+                d["description"] = row[2]
+                d["type"] = row[3]
+                d["plataform"] = row[4]
+                d["link"] = row[5]
+                res["bots"].append(d)
+
+            return res
+        except Exception as e:
+            print(e)
+            res['error'] = True
+            res['message'] = 'Erro ao retornar bots'
+
+            return res
+    
+    def update(self, data, bot_id):
+        ConnectDB().connect()
+        res = {
+            'error': False
+        }
+
+
+        title = data['title']
+        description = data['description']
+        type = data['type']
+        plataform = data['plataform']
+        link = data['link']
+
+        command = f'UPDATE bots SET title = "{title}", description = "{description}", type = "{type}", plataform = "{plataform}", link = "{link}" WHERE id = {bot_id}'
+
+        try:
+            ConnectDB().exec_DML(sql=command)
+
+            res['bot'] = self.show(bot_id=bot_id)
+
+            return res
+        except Exception as e:
+            print(e)
+            res['error'] = True
+            res['message'] = 'Erro ao criar bot'
+
+            return res
+
+    def delete(self, bot_id):
+        ConnectDB().connect()
+        res = {
+            'error': False
+        }
+        command = f'DELETE FROM bots WHERE id = {bot_id}'
+
+        try:
+            ConnectDB().exec_DML(sql=command)
+
+            res["message"] = f'Bot {bot_id} deletado com sucesso'
+            return res
+        except Exception as e:
+            print(e)
+            res['error'] =  True
+            res['message'] = 'Erro ao deletar bot'
+
+            return res
--- routes/bots.py
+++ routes/bots.py
@@ -6,9 +6,85 @@ from views import helper
 
 @app.route('/bots/download', methods=['POST'])
 @helper.access_required
-def download(user):
+def download_bots(user):
     try:
         response = BotController().download()
         return make_response(response, 200)
     except Exception as e:
         return make_response({'message': 'Erro interno'}, 500)
+
+@app.route('/bots', methods=['GET'])
+def show_bots():
+    bot_id = request.args.get('id')
+
+    try:
+        if bot_id:
+            result = BotController().show(bot_id=bot_id)
+        else:
+            result = BotController().index()
+
+        if result['error']:
+            return make_response({
+                'message': result['message']
+            }, 401)
+
+        return make_response(result, 200)
+    except Exception as e:
+        
+        print(e)
+        return make_response({'message': 'Erro interno'}, 500)
+
+@app.route('/bots', methods=['POST'])
+@helper.admin_required
+def create_bots():
+  request_data = request.get_json()
+
+  try:
+    result = BotController().create(data=request_data)
+
+    if result['error']:
+        return make_response({
+            'message': result['message']
+        }, 401)
+
+    return make_response(result, 201)
+  except Exception as e:
+    print(e)
+    return make_response({'message': 'Erro interno'}, 500)
+
+@app.route('/bots', methods=['DELETE'])
+@helper.admin_required
+def delete_bots():
+  bot_id = request.args.get('id')
+
+  try:
+    result = BotController().delete(bot_id=bot_id)
+
+    if result['error']:
+            return make_response({
+                'message': result['message']
+            }, 401)
+
+    return make_response(result, 200)
+  except Exception as e:
+    print(e)
+    return make_response({'message': 'Erro interno'}, 500)
+
+@app.route('/bots', methods=['PUT'])
+@helper.admin_required
+def update_bots():
+  bot_id = request.args.get('id')
+  request_data = request.get_json()
+
+  try:
+    result = BotController().update(bot_id=bot_id, data=request_data)
+
+    if result['error']:
+        return make_response({
+            'message': result['message']
+        }, 401)
+
+    return make_response(result, 201)
+  except Exception as e:
+    print(e)
+    return make_response({'message': 'Erro interno'}, 500)
